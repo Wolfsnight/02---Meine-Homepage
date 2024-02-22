36ab7aa2c71288bebeea4fb6e555e9af288c9973
Markus Spiegler <Spiegler@hotmail.de>
2024-02-22 16:07:32 +0100

    UPDATE - 2024-02-22 - 16:00

--- asssets/scripts/script.js
+++ asssets/scripts/script.js
@@ -1,3 +1,32 @@
+//  Menü  /////////////////////////////////////////////////
+
+const navDisplay = () =>
+{
+    const nav = document.getElementById("header-nav");
+    const menuOpen = document.getElementById("menu-open");
+    const menuClose = document.getElementById("menu-close")
+
+    if (nav.style.display === "none")
+    {
+        nav.style.display = "unset";
+        menuOpen.style.display = "none";
+        menuClose.style.display = "unset";
+    } else
+    {
+        nav.style.display = "none";
+        menuOpen.style.display = "unset";
+        menuClose.style.display = "none";
+    }
+}
+
+
+
+
+document.getElementById("menu-icon").addEventListener("click", menu)
+
+
+//  Impressum  ////////////////////////////////////////////
+
 const impressum_close = () =>
 {
     document.getElementById("impressum").style.display = "none"
